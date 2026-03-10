0367ab0e01111eb085ce09c6a0525a4a321866f1
kareem45726 <kareemullah03419267719@gmail.com>
2026-03-11 00:10:21 +0500

    Create Lab 1 task 01.cpp

---
+++ Lab 1/lab 1 tasks/Lab 1 task 01.cpp
@@ -0,0 +1,15 @@
++ @@ -0,0 +1,18 @@
++ // Name: kareem ullah
++ // Department: Electrical Engineering (Power)
++ // Registration No: BF25NWELE0721
++ // Section: A
+
++ // This is my first C++ program to display a message
+
++ #include <iostream>   // Used for input and output
++ using namespace std;  // Using standard namespace
++ int main()
+{
+  + // Printing message on the screen   
+    return 0;  // Program ends successfully
+}
