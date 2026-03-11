9c3c48037308185e1aa6d9040fc52c4a5b58c864
kareem45726 <kareemullah03419267719@gmail.com>
2026-03-11 23:29:37 +0500

    Add files via upload

---
+++ lab 5/lab work/LAB_05 LabWork_1-CPP.cpp
@@ -0,0 +1,22 @@
+// Name: Kareem ullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0721
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    while(i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
+
+
