dcce87f983eeceee39b416c3bc6c7e2e73a310d7
Kareem Kohii <kareemullah03419267719@gmail.com>
2026-05-14 17:10:21 +0500

    Add files via upload

---
+++ Lab 11/Lab Work/Lab Work_2.cpp
@@ -0,0 +1,24 @@
+// Name: Kareem Ullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0721
+// Section: A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring and initializing first name
+    string firstName = "Kareem";
+
+    // Declaring and initializing last name
+    string lastName = "Ullah";
+
+    // Concatenating first name and last name with a space in between
+    string fullName = firstName + " " + lastName;
+
+    // Displaying the full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
